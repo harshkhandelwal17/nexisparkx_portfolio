8e6bd3957bf189e2872fee45a5807355263c602c
harsh khandelwal <harshkhandelwal597@gmail.com>
2025-02-19 19:33:03 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export', // Ensure you are using this if deploying a static site
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
+  
